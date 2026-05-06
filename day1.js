d2b022fa376f37e0edaf53933b398f784d656fae
HariKrishnanM9 <krishharishm18@gmail.com>
2026-05-06 19:37:54 +0530

    new task

--- day1.js
+++ day1.js
@@ -1,1 +1,6 @@
 console.log(20);
+console.log("Hello world");
+let fistProgram = "hello world";
+a = fistProgram
+s = a
+console.log(s);
---
+++ main/exe.js
@@ -0,0 +1,10 @@
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// console.log(a,b, c);
+
+var a = 10;
+var b = 20;
+var c = a + b;
+var d = b - a;
+console.log(c);
